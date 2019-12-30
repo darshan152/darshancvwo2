a9b4328b05e123450a4e32c9186ede8c551d2947
Darshan <darshan152151@hotmail.com>
2019-12-30 17:14:29 +0800

    Improved formatting

--- app/javascript/components/PostList.js
+++ app/javascript/components/PostList.js
@@ -1,109 +1,105 @@
 import React, { useEffect, useState } from "react";
 import { navigate } from "@reach/router";
-import { array } from "prop-types";
+
 
 
 
 function PostList() {
-    const [posts, setPosts] = useState([]);
-    let [wuru, setWuru] = useState([]);
+  const [posts, setPosts] = useState([]);
+  let [display, setDisplay] = useState([]);
     
     
-    useEffect(() => {
-      const requestPosts = async () => {
-        const response = await fetch("/api/posts");
-        const { data } = await response.json();
-        setPosts(data);
-        displayposts("All");
-      };
-      requestPosts();
-    }, []);
+  useEffect(() => {
+    const requestPosts = async () => {
+      const response = await fetch("/api/posts");
+      const { data } = await response.json();
+      setPosts(data);
+    };
+    requestPosts();
+  }, []);
     
-    
+  
   var categories = new Array();
   posts.map(post=>{
     categories.push(post.attributes.body)
   });
   let uniquecats =[...new Set(categories)];
-  console.log(uniquecats)
   
   let options=uniquecats.map(cat=>{
     return (
-    <option value={cat}>{cat}</option>
+      <option value={cat}>{cat}</option>
     )
-  }
-  );
-  console.log(options)
+  });
   
   function filteredposts(cat){
-    console.log(posts + "Lsd")
     if(cat=="All"){
       return(
         posts
       )
-
     }else{
-    return(
-      posts.filter(post=>post.attributes.body==cat)
-      
+      return(
+       posts.filter(post=>post.attributes.body==cat)
       )
-    }}
+    }
+  }
       
   
   function displayposts(cat) {
-        let quvu = cat == 'All' ? cat : cat.target.value
-        console.log(quvu)
-
-        let abrakadabra = filteredposts(quvu).map(post => {
-
-        let wrap;
-        let donebutton;
-        if (post.attributes.done) {
+    let displayedcategory = cat == 'All' ? cat : cat.target.value
+    let displayedpost = filteredposts(displayedcategory).map(post => {
+      let wrap;
+      let donebutton;
+      if (post.attributes.done) {
         wrap = <p><del>{post.attributes.title}</del></p>
-        donebutton=<button onClick={()=> navigate('/undo/'+post.id)}>
-        Undo
-      </button>
+        donebutton=
+        <button onClick={()=> navigate('/undo/'+post.id)}>
+          Undo
+       </button>
       } else {
         wrap = <p>{post.attributes.title}</p>
-        donebutton=<button onClick={()=> navigate('/done/'+post.id)}>
+        donebutton=
+        <button onClick={()=> navigate('/done/'+post.id)}>
         Done
-      </button>
+        </button>
       }
     
       return(
-      <div>
-      <div>{wrap}</div>
-        {donebutton}
-      <button onClick={()=> navigate('/edit/'+post.id)}>
-        Edit
-      </button>
-      <button onClick={()=> navigate('/del/'+post.id)}>
-        Delete
-      </button>
-      </div>
+        <div>
+          <div>{wrap}</div>
+          {donebutton}
+          <button onClick={()=> navigate('/edit/'+post.id)}>
+            Edit
+          </button>
+          <button onClick={()=> navigate('/del/'+post.id)}>
+            Delete
+          </button>
+        </div>
       )
-  })
+    })
 
-  setWuru(abrakadabra);
-};
+    setDisplay(displayedpost);
+  };
 
 
 
-console.log(wuru)
+
   return (
-    
-  <div>
-   	<h2>To-Do List</h2>
-     <select onChange={displayposts} id="filterval">
+    <div>
+   	  <h2>To-Do List</h2>
+      <select onChange={displayposts} id="filterval">
+        <option>Choose a Category</option>
         <option value="All">All</option>
         {options}
       </select>
-    
-    <button onClick={() => navigate('/add')}>
-  	Add
-  	</button>
-    {wuru}
-  	</div>
+      <br />
+      
+      {display}
+      
+      <br />
+      <button onClick={() => navigate('/add')}>
+  	    Add
+  	  </button>
+    </div>
   );
 }
 
